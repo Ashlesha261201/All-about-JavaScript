6c9c7fd5f96af90193c53ae0064ac9bfbef06b47
Ashlesha261201 <ashleshakavthanker4287@gmail.com>
2025-06-27 16:57:09 +0530

    learnings of 27-06-2025

---
+++ online lect of JS/loops/app.js
@@ -0,0 +1,51 @@
+// loops 
+
+//For loop:
+
+// --to display number from 1- 14 using for loop
+
+
+// for (let i =1; i <= 14 ; i++){
+//     console.log(i);   
+// }
+
+// While loop:
+
+let count = 1;   // initial condition
+while (count <= 15) // termintion condition
+{
+    console.log(count);
+    count++;  // incremental condition
+}
+
+// --------------------------
+let number = 14;
+while (number >= 3){
+    console.log(number);
+    number--;
+}
+
+// Do-while  
+
+let d = 1000;
+
+do {console.log(d);
+    d++;
+}
+
+while (d<=10);
+
+// for ---> minimum no. of time this will run ----> 0
+// while ---> minimum no. of time this will run ---->0
+// do while ---> minimum no. of time this will run ----> 1
+
+
+//pre and post increment example
+let a = 10;
+
+console.log(a);
+// console.log(a++);
+console.log(++a);
+
+
+
---
+++ online lect of JS/strings/string.js
@@ -0,0 +1,19 @@
+
+// let a = "Ashlesha"
+// let b = 'Ashlesha'
+// let c = `Ashlesha`
+// // let d = new String ("Ashlesha")    //should never use
+// console.log(a, b, c, d);
+
+// -----------------------------------------------------
+
+// hey i am ashlesha kavthanka, i am 23 years old and i stay in mumbai and i am a developer
+
+let naam = "Ashlesha Kawthankar";
+let age = 23;
+let state = "Mumbai";
+let profession = "Developer";
+
+//.. (concatenation)-------> adding two strings i.e a+b = ab
+
+console.log(" hey i am "+ naam + " I am " + age + " years old and i stay in " + state + " and i am a " + profession);
