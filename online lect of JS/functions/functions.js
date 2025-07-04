28548e603b4d5900717b7a04653fcd79477bcbd6
Ashlesha261201 <ashleshakavthanker4287@gmail.com>
2025-07-04 22:45:50 +0530

    learning from 04/07/25 part 3

---
+++ online lect of JS/functions/functions.js
@@ -0,0 +1,35 @@
+// function something (){}  // syntax of writing a function
+
+
+// {} ==> they are called block -- whatever you will write here will be exceuted when we'll call the function
+// funstion defination
+function something (){
+    console.log("i am inside swarg ka darwaja");
+    
+}  
+something(); //fn calling 
+something();
+something();
+something();
+
+//we can call function as many time as we want
+
+//-------------------------------------------
+
+// function ashesha (){
+// console.log("hello, Namaste");
+//  return true;
+// }
+
+// let output = ashesha(); // if we dont return something in the above block of code it will return undefined.
+// console.log(output);
+
+//-------------------------------------------
+
+function notebook (price){
+console.log(price);
+
+}
+
+notebook(55) //arguments
+
