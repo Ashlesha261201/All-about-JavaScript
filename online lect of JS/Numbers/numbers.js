85733747e74a3f8d9793f4cc613a6fc41af7eb7b
Ashlesha261201 <ashleshakavthanker4287@gmail.com>
2025-07-04 20:59:07 +0530

    learning of 04/07/25 part 2

--- online lect of JS/Numbers/numbers.js
+++ online lect of JS/Numbers/numbers.js
@@ -13,3 +13,18 @@ console.log(Math.ceil(3.999));
 
 console.log(Math.max(12,55,44,88,33,456,2515,541,54,54,5,485,45,));
 console.log(Math.min(12,55,44,88,33,456,2515,541,54,54,5,485,45,));
+
+// ---------------------------------------------------------------
+console.log( Math.random() ); //0 - 1 where 0 is incluede but not 1 
+console.log( Math.random() * 10 ); //(0- 10) ==> range = 10  - 0 (multiplication)
+
+
+console.log( Math.random()* 95); 
+console.log( Math.floor(Math.random()* 95)); 
+
+console.log( Math.floor(Math.random()* 95) + 3);   //here it can never go less than 3 and would never include 98
+
+console.log(Math.floor(Math.random()* 90000) + 10000);
+
+
+
