65093b0ece93bc89cd6584cace7c7ef7cdc1db4a
Ashlesha261201 <ashleshakavthanker4287@gmail.com>
2025-07-14 22:26:10 +0530

    laerning of 14/07/25

--- online lect of JS/coding-practice/coding.js
+++ online lect of JS/coding-practice/coding.js
@@ -2,20 +2,20 @@
  
 // --> it helps to transform original array 
 
-let arr = [1,2,3,4,5]
-//  for (i=0;i<arr.length;i++){
-//     arr [i] = 2 * arr[i]
-//  }
-// console.log(arr);
+// let arr = [1,2,3,4,5]
+// //  for (i=0;i<arr.length;i++){
+// //     arr [i] = 2 * arr[i]
+// //  }
+// // console.log(arr);
 
-console.log(arr.map(num => 2 * num));
+// console.log(arr.map(num => 2 * num));
 
 
 // --------------------filter ()-------------------------------------
 
 // from the callback condition it wil filter the array and will return the array which will only have elements that satisfy the condition of call back 
 
-let array = [1,2,3,4,5]
+// let array = [1,2,3,4,5]
 
 // for (i=0;i<array.length;i++){       traditional way 
 //     if (array[i]% 2 == 0){
@@ -26,11 +26,51 @@ let array = [1,2,3,4,5]
 // }
 
 
-console.log(array.filter( item => item % 2 == 0));
+// console.log(array.filter( item => item % 2 == 0));
 
 
 // --------------------reduce ()-------------------------------------
 
 //helps to console the 
 
-let 
+// x = 5;
+// function outer(){
+//     let x = 10;
+//     function inner (){
+//         console.log(x);
+        
+//     }
+//     inner();
+// }
+// outer();
+
+// function greet(name){
+//     console.log(`hello, ${name}!`);
+    
+// }
+// 
+// greet("name");
+// greet("bob");9
+
+
+
+// let x = 5;
+// function increment(num){
+//     num += 1;
+//     return num
+// }
+
+// x = increment(x);
+
+
+let counter = 0;
+function increment(){
+    counter += 1;
+
+}
+
+increment();
+increment();
+increment();
+console.log(counter);
+
---
+++ online lect of JS/scopes/scopes.js
@@ -0,0 +1,159 @@
+
+
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// if (true){
+//     var d = 100;
+//     let b = 200;
+//     const c = 300;
+// console.log(d);
+// console.log(b);
+// console.log(c);
+// }
+
+// function ash(){
+//     var a = 1000;
+//     let b = 2000;
+//     const c = 3000;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// }
+// ash();
+
+
+// ---------------------------------------------------
+
+// var a = 20;
+// console.log(a);
+
+// function ash(){
+//     var b =200;
+//     console.log(b);
+    
+// }
+// ash();
+
+// ---------------------------
+// console.log(a);
+// var a = 20;
+
+// ash();
+// function ash(){
+//     var b =200;
+//     console.log(b);
+    
+// }
+
+
+// -=--------------------------
+
+// console.log(a);
+// var a = 20;
+
+// ash();
+// function ash(){
+//    console.log(b);
+//     var b =200; 
+// }
+// -=--------------------------
+
+// console.log(a);
+// let a = 20;
+
+// ash();
+// function ash(){
+//    console.log(b);
+//     let b =200; 
+// }
+// -=--------------------------
+
+
+
+// let a = 20;
+// console.log(a);
+
+// ash();
+// function ash(){
+//    console.log(b);
+//     let b =200; 
+// }
+
+// -=--------------------------
+
+// var a = 10;
+// var a = true
+// console.log(a);
+
+// -=--------------------------
+
+// let a = 10;
+// let  a = true
+// console.log(a);
+
+// -=--------------------------
+
+
+// const a = 10;
+// const  a = true
+// console.log(a);
+
+// -=--------------------------
+
+
+// let a = 10;
+//   a = true
+// console.log(a);
+
+// -=--------------------------
+
+// const a = 10;
+//   a = true
+// console.log(a);
+
+// -=--------------------------
+
+// const a  ;
+//   a = true
+// console.log(a);
+
+// -=--------------------------
+
+// var a = 10;
+// function sam(){
+//     var a =20
+//     console.log(a);
+    
+// }
+// sam();
+
+// console.log(a);
+// -=--------------------------
+
+// var a = 10;
+// if(true){
+//     var a =20
+//     console.log(a);
+    
+// }
+
+// console.log(a);
+
+// -=--------------------------
+
+var a = 10;
+if(false){
+    var a =20
+    console.log(a);
+    
+}
+
+console.log(a);
